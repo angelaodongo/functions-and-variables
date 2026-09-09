95938a5ff14f4aab6b2c1c295a632d2da1a46ca2
Angela Odongo <celesteodongo@gmail.com>
2026-09-09 11:54:17 +0300

    split the user's name into first name and last name

--- functions_&_variables.py
+++ functions_&_variables.py
@@ -4,5 +4,8 @@ name = input("What is your name? ").strip().title()
 #Capitalize first letter of string
 #name = name.capitalize()
 
+#split user's name into first name and last name
+first, last = name.split(" ")
+
 # output
-print ("Hello", name, sep=' -> ', end='!')
+print (f"Hello, {first}")
