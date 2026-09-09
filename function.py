977f6ac9ed72dffc2ef071989935c43c8be61c93
Angela Odongo <celesteodongo@gmail.com>
2026-09-09 13:00:49 +0300

    added a comment

--- function.py
+++ function.py
@@ -1,3 +1,5 @@
+#when you call a function, make sure the function is already defined, otherwise the interpreter will throw in an error
+
 def hello(personName = "World"): #default parameter: incase the programmer doesn't specify a parameter when calling the function
     print("Hello", personName)
 
