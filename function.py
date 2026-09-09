7ebefeda8bb58e51d5fa0153ed35cce10fbb3aa3
Angela Odongo <celesteodongo@gmail.com>
2026-09-09 13:13:22 +0300

    added a comment regarding the order in which to write user functions and defining and calling main() the introduced a new function def main() to call the user function, then called main at the end

--- function.py
+++ function.py
@@ -1,8 +1,13 @@
-#when you call a function, make sure the function is already defined, otherwise the interpreter will throw in an error
+# when having user-defined functions, follow this order:
+#   ->define your user function
+#   ->define the main, where you call the user defined function(s)
+#   ->call main()
 
 def hello(personName = "World"): #default parameter: incase the programmer doesn't specify a parameter when calling the function
     print("Hello", personName)
 
-name = input("Enter your name: ")
-hello()
-hello(name)
+def main():  
+    name = input("Enter your name: ")
+    hello(name)
+
+main()
