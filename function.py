9f2773fb8a4c67adfaa3f9750748375d905cae48
Angela Odongo <celesteodongo@gmail.com>
2026-09-09 12:59:41 +0300

    added a default parameter to the function

--- function.py
+++ function.py
@@ -1,5 +1,6 @@
-def hello(personName):
+def hello(personName = "World"): #default parameter: incase the programmer doesn't specify a parameter when calling the function
     print("Hello", personName)
 
 name = input("Enter your name: ")
+hello()
 hello(name)
