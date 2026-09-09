84ba775d5930311846923d142059d648d1c22da3
Angela Odongo <celesteodongo@gmail.com>
2026-09-09 23:31:01 +0300

    created code that converts uppercase to lowercase

--- indoor.py
+++ indoor.py
@@ -1,3 +1,5 @@
 # The program is supposed to prompt the user for input and then outputs the same input in lowercase.
 # Punctuation and whitespace should be outputted unchanged.
 
+phrase = input("Enter a phrase: ").lower()
+print(phrase)
