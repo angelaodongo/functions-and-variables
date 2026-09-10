ce47bd5b7a69e612caeaaf06d4d30e0a3113b47b
Angela Odongo <celesteodongo@gmail.com>
2026-09-10 14:42:43 +0300

    created a new file called einstein.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def convert(f):
-    f = f.replace(':)', '🙂').replace(':(', '🙁')
-    return f
-
-def main():
-    phrase = str(input("Enter a phrase with emoji: "))
-    print("->", convert(phrase))
-
-main()
