b0dd220957337b5a9e420c0468141cd955dee56f
Angela Odongo <celesteodongo@gmail.com>
2026-09-12 17:22:48 +0300

    created the function redact, which takes in 2 parameters, s & w, where the sentence s is scanned for the particular word which the user wants redacted, then replaced with [REDACTED]

--- redact.py
+++ redact.py
@@ -1,2 +1,7 @@
 # Implement a function called redact that accepts a str sentence and a str word to redact, and returns the sentence with every occurrence of that word replaced by "[REDACTED]".
 # Implement main, which prompts the user for a sentence and a word, calls redact, and prints the result.
+
+def redact(s, w):
+    s = s.replace(w, '[REDACTED]')
+    return s
+
