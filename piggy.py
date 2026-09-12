3cf11b695baa076eafd9e8db06b398b51def135c
Angela Odongo <celesteodongo@gmail.com>
2026-09-12 16:20:28 +0300

    added comments to describe problem requirements

---
+++ piggy.py
@@ -0,0 +1,3 @@
+# Implement a function called coins_to_dollars that accepts a number of coins (an int, assume all quarters, each worth 25 cents) and returns the equivalent dollar amount as a float, formatted to 2 decimal places (e.g., 6 quarters → 1.50). 
+# Implement main, which prompts the user for a number of coins, calls coins_to_dollars, and prints the result like "$1.50"
+
