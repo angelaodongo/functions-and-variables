c952cf82d2f48296c584b43de47efb9cbed2f763
Angela Odongo <celesteodongo@gmail.com>
2026-09-12 12:55:57 +0300

    created the function shout, which is supposed to take input from main and store it in variable s. Then the variable upperCased changed the input to uppercase letters. Then explamation concatenates the output of upperCased and adds an exclamation(!). the function returns exclamation when completed.

--- shout.py
+++ shout.py
@@ -2,3 +2,7 @@
 # Accepts a string and returns it in uppercase with an exclamation mark
 # main prompts the user for input, calls shout and prints the result
 
+def shout(s):
+    upperCased = s.upper()
+    exclamation = f"{upperCased}", "!"
+    return exclamation
