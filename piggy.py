552c8b0b85e8edf7d4026c83a000639900a88dc1
Angela Odongo <celesteodongo@gmail.com>
2026-09-12 16:27:22 +0300

    created the function coins_to_dollars which takes the input c, multiplies by 0.25 then formats the value into a 2dp float with $ at the front

--- piggy.py
+++ piggy.py
@@ -1,3 +1,9 @@
-# Implement a function called coins_to_dollars that accepts a number of coins (an int, assume all quarters, each worth 25 cents) and returns the equivalent dollar amount as a float, formatted to 2 decimal places (e.g., 6 quarters → 1.50). 
+# Implement a function called coins_to_dollars that accepts a number of coins (an int, assume all quarters, each worth 25 cents) 
+# Returns the equivalent dollar amount as a float, formatted to 2 decimal places (e.g., 6 quarters → 1.50). 
 # Implement main, which prompts the user for a number of coins, calls coins_to_dollars, and prints the result like "$1.50"
+
+def coins_to_dollars(c):
+    d = c * 0.25
+    convertedValue = f"${float(d):.2f}"
+    return convertedValue
 
