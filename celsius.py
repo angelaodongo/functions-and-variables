f7aede81f6c7d1067670ec17d45fe1349d4a87af
Angela Odongo <celesteodongo@gmail.com>
2026-09-12 15:45:00 +0300

    added comments to describe the problem requirements

---
+++ celsius.py
@@ -0,0 +1,4 @@
+# Implement a function called convert that accepts a temperature in Fahrenheit (as a float) and returns the equivalent in Celsius, rounded to 1 decimal place. 
+# (Formula: C = (F - 32) * 5/9.) 
+# Implement main, which prompts the user for a Fahrenheit temperature, calls convert, and prints the result formatted like "25.0 C".
+
