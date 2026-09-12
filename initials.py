85c002234b52824c053b2f01313cf34677021a73
Angela Odongo <celesteodongo@gmail.com>
2026-09-12 12:48:49 +0300

    utilised the split function, and realised that it splits a sting into 2 strings in one list. Also the f string method is used to concatenate two elements into one

--- initials.py
+++ initials.py
@@ -2,3 +2,14 @@
 # Implement a function called get_initials that accepts the full name as a str and returns the initials in UPPERCASE, formatted like "J.S.", with periods no spaces
 # Call get_initials from main and print the result
 
+def get_initials(n):
+    firstInitial = n.split()[0][0]
+    secondInitial = n.split()[1][0]
+    initials = f"{firstInitial}.{secondInitial}.".upper()
+    return initials
+
+def main():
+    personName = input("Enter your name: ")
+    print(get_initials(personName))
+
+main()
