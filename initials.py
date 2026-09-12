61dff83387aabff817b5f9d45509e0b3624170cd
Angela Odongo <celesteodongo@gmail.com>
2026-09-12 12:08:16 +0300

    added comments to list problem requirements

---
+++ initials.py
@@ -0,0 +1,4 @@
+# Prompt the user for their full name (Assume first and last name only, separated by a space)
+# Implement a function called get_initials that accepts the full name as a str and returns the initials in UPPERCASE, formatted like "J.S.", with periods no spaces
+# Call get_initials from main and print the result
+
